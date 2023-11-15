58fef8712d98d0f18136aaf0f92829541c073c3a
Matheus Dario <dario.matheus97@gmail.com>
2023-11-15 18:38:12 -0300

    basic configs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../styles/global-styles';
+import { theme } from '../styles/theme';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
