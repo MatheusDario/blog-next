bb4f17ae407458576362424fe9ce12ccd14b185f
Matheus Dario <dario.matheus97@gmail.com>
2023-11-16 14:04:50 -0300

    about site estatico with getStaticProps

---
+++ src/domain/posts/post.ts
@@ -0,0 +1,92 @@
+export type PostID = number;
+
+export type AuthorAttributesData = {
+  name: string;
+  created_by: number;
+  updated_by: number;
+  published_by: number;
+  created_at: string;
+  updated_at: string;
+  published_at: string;
+};
+
+export type PostAuthor = {
+  data: {
+    id: PostID;
+    attributes: AuthorAttributesData;
+  };
+};
+
+export type CategoryAttributesData = {
+  name: string;
+  created_by: number;
+  updated_by: number;
+  published_at: string;
+};
+
+export type PostCategory = {
+  data: {
+    id: PostID;
+    attributes: CategoryAttributesData;
+  };
+};
+
+export type PostCreatedBy = {
+  id: PostID;
+  firstname: string;
+  lastname: string;
+  username: null;
+};
+
+export type PostCoverFormat = {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: null;
+  size: number;
+  width: number;
+  height: number;
+  provider_metadata: {
+    public_id: string;
+    resource_type: string;
+  };
+};
+
+export type PostCover = PostCoverFormat & {
+  id: PostID;
+  alternativeText: string;
+  caption: string;
+  previewUrl: null;
+  provider: string;
+  created_by: number;
+  updated_by: number;
+  created_at: string;
+  updated_at: string;
+  formats: {
+    thumbnail: PostCoverFormat;
+    small: PostCoverFormat;
+    medium: PostCoverFormat;
+    large: PostCoverFormat;
+  };
+};
+
+export type PostAttributesData = {
+  title: string;
+  content: string;
+  slug: string;
+  author: PostAuthor;
+  category: PostCategory;
+  created_by: PostCreatedBy;
+  updated_by: PostCreatedBy;
+  created_at: string;
+  updated_at: string;
+  published_at: string;
+  cover: PostCover;
+};
+
+export type PostData = {
+  id: PostID;
+  attributes: PostAttributesData;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <h1>Hello World!</h1>;
+import { useEffect, useState } from 'react';
+import { PostData } from '../domain/posts/post';
+import { GetStaticProps } from 'next';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(
+    'https://myblog-strapi.onrender.com/api/posts?populate=*',
+  );
+  const jsonPosts = await posts.json();
+  return jsonPosts.data;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  //const [posts, setPosts] = useState<PostData[]>([]);
+
+  //useEffect(() => {
+  //getPosts().then((response) => setPosts(response));
+  //}, [posts]);
+
+  console.log(posts);
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.id}>{post.attributes.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: { posts },
+  };
+};
