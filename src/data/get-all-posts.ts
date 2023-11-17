48872f01b57e197426bd1ff746ca497963ca3385
Matheus Dario <dario.matheus97@gmail.com>
2023-11-17 16:32:02 -0300

    add query in get-all-posts

--- src/data/get-all-posts.ts
+++ src/data/get-all-posts.ts
@@ -2,7 +2,8 @@ import { POSTS_URL } from '@/config/app-config';
 import { PostData } from '@/domain/posts/post';
 import { fetchJson } from '@/utils/fetch-json';
 
-export const getPosts = async (): Promise<PostData[]> => {
-  const post = fetchJson<PostData[]>(POSTS_URL);
+export const getPosts = async (query = ''): Promise<PostData[]> => {
+  const url = `${POSTS_URL}&${query}`
+  const post = fetchJson<PostData[]>(url);
   return post;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home({ posts }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getPosts();
+  const posts = await getPosts('sort=id:desc&pagination[start]=0&pagination[limit]=10');
 
   return {
     props: { posts },
