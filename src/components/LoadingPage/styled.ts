ede6580347ab151964714ceaffa021fecbe73127
Matheus Dario <dario.matheus97@gmail.com>
2023-11-21 16:55:32 -0300

    update loadingPage

--- src/components/LoadingPage/styled.ts
+++ src/components/LoadingPage/styled.ts
@@ -2,10 +2,13 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   ${({ theme }) => css`
+    height: 100vh;
     display: flex;
     text-align: center;
+    flex-direction: column;
     justify-content: center;
-    color: ${theme.colors.secondary};
+    background-color: black;
+    color: ${theme.colors.white};
     font-size: ${theme.font.sizes.large};
   `}
 `;
