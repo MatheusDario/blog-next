59114864fcbf8550fa4220cdac979f51619ce92e
Matheus Dario <dario.matheus97@gmail.com>
2023-11-23 15:13:05 -0300

    update url link api

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'https://myblog-strapi.onrender.com/api/posts';
+//export const API_URL = 'https://myblog-strapi.onrender.com/api/posts';
+export const API_URL = 'https://apiblog.matheusdario.com.br/api/posts'
 export const POSTS_URL = `${API_URL}/?populate=*`
 export const POST_SLUG = `${POSTS_URL}&filters[slug][$eq]=`
 
